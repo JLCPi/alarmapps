bb1df0390cc2ccb563c33c735521dc42f48c03db
JLCPi <echidna.the.witch.of.tea@gmail.com>
2026-01-19 22:43:49 +0300

    Update installalarmapps.sh

--- installalarmapps.sh
+++ installalarmapps.sh
@@ -7,4 +7,15 @@ mkdir alarmapps
 cd alarmapps
 wget https://github.com/JLCPi/alarmapps/raw/refs/heads/main/test/alarmapps.sh
 chmod +x alarmapps.sh
-echo alarmapps is now installed, some apps might not work, expect bugs and errors.
+echo Creating menu icon...
+echo "[Desktop Entry]
+Name=AlarmApps
+Comment=Arch Linux Arm App Store                         
+Exec=${HOME}/alarmapps/alarmapps.sh
+Icon=                           
+Terminal=false
+StartupWMClass=alarmapps
+Type=Application
+Categories=Utility;System;PackageManager;
+StartupNotify=true" > ~/.local/share/applications/alarmapps.desktop
+echo AlarmApps is now installed, some apps might not work, expect bugs and errors.
